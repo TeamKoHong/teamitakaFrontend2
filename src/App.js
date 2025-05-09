1a30d4fe13f32cdba33656df5f919b6240513b0d
Hyun Sang <pycandyq@naver.com>
2025-05-09 16:18:55 +0900

    프로젝트 상세 페이지 라우터 추가

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ProjectManagement from "./pages/ProjectManagement/ProjectManagement";
 import "./App.css";
+import ProjectDetailPage from "./pages/ProjectDetailPage/ProjectDetailPage";
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/project-management" element={<ProjectManagement />} />
+        <Route path="/project/:id" element={<ProjectDetailPage />} />
       </Routes>
     </Router>
   );
