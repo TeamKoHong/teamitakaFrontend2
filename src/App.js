f62ab96fd407fea1204b4965682cbd126aa700df
Hyun Sang <pycandyq@naver.com>
2025-05-16 19:41:14 +0900

    react-spring-bottom-sheet 라이브러리 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ProjectManagement from "./pages/ProjectManagement/ProjectManagement";
 import "./App.css";
 import ProjectDetailPage from "./pages/ProjectDetailPage/ProjectDetailPage";
+import "react-spring-bottom-sheet/dist/style.css";
 const App = () => {
   return (
     <Router>
