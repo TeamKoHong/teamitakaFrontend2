4fe5f624bb23678767a8b2addc8fa687b5229916
Hyun Sang <pycandyq@naver.com>
2025-05-21 17:54:47 +0900

    파일 경로 오류 수정

--- src/pages/ProjectDetailPage/ProjectDetailPage.js
+++ src/pages/ProjectDetailPage/ProjectDetailPage.js
@@ -1,9 +1,9 @@
-import MainFloatingBox from "../../components/\bProjectDetailPage/MainFloatingBox";
-import ProjectDetailHeader from "../../components/\bProjectDetailPage/ProjectDetailHeader";
-import TodoBox from "../../components/ProjectDetailPage/TodoBox";
-import ProjectDetailSlideBox from "../../components/ProjectDetailPage/ProjectDetailSlideBox";
+import MainFloatingBox from "../../components/ProjectDetailPage/MainFloatingBox";
+import ProjectDetailHeader from "../../components/ProjectDetailPage/ProjectDetailHeader";
+import TodoBox from "../../components/ProjectDetailPage/TodoBox";
+import ProjectDetailSlideBox from "../../components/ProjectDetailPage/ProjectDetailSlideBox";
 import "./ProjectDetailPage.scss";
-import TimeLineBox from "../../components/\bProjectDetailPage/TimeLineBox";
+import TimeLineBox from "../../components/ProjectDetailPage/TimeLineBox";
 function ProjectDetailPage() {
   return (
     <div className="project-detail-page-container">
