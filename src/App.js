eefcdd9a15eca5006cce4c5c79d6b5a1de74887b
Hyun Sang <pycandyq@naver.com>
2025-05-21 17:51:47 +0900

    각 버튼마다 페이지 추가

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import ProjectManagement from "./pages/ProjectManagement/ProjectManagement";
 import "./App.css";
 import ProjectDetailPage from "./pages/ProjectDetailPage/ProjectDetailPage";
 import "react-spring-bottom-sheet/dist/style.css";
+import ProjectMemberPage from "./pages/ProjectMemberPage/ProjectMemberPage";
+import ProceedingsPage from "./pages/ProceedingsPage/ProceedingsPage";
+import ProjectVotePage from "./pages/ProjectVotePage/ProjectVotePage";
+import ProjectCalender from "./pages/ProjectCalendar/ProjectCalendar";
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/project-management" element={<ProjectManagement />} />
         <Route path="/project/:id" element={<ProjectDetailPage />} />
+        <Route path="/project/:id/member" element={<ProjectMemberPage />} />
+        <Route path="/project/:id/proceedings" element={<ProceedingsPage />} />
+        <Route path="/project/:id/vote" element={<ProjectVotePage />} />
+        <Route path="/project/:id/calender" element={<ProjectCalender />} />
       </Routes>
     </Router>
   );
