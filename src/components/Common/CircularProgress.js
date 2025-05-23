a37bed54c55cc9de2cba88a87e0500060ed9c131
Hyun Sang <pycandyq@naver.com>
2025-05-23 17:59:46 +0900

    원형 프로게스바 공통컴포넌트로 분리

---
+++ src/components/Common/CircularProgress.js
@@ -0,0 +1,50 @@
+// 원형 프로그레스 바
+const CircularProgress = ({ percentage }) => {
+  const radius = 20;
+  const strokeWidth = 4;
+  const circumference = 2 * Math.PI * radius;
+  const offset = circumference - (percentage / 100) * circumference;
+
+  return (
+    <svg width="64" height="64" viewBox="0 0 44 44">
+      {/* 배경 원 */}
+      <circle
+        cx="22"
+        cy="22"
+        r={radius}
+        fill="none"
+        stroke="#eee"
+        strokeWidth={strokeWidth}
+      />
+      {/* 진행 바 */}
+      <circle
+        cx="22"
+        cy="22"
+        r={radius}
+        fill="none"
+        stroke="#F76241"
+        strokeWidth={strokeWidth}
+        strokeDasharray={circumference}
+        strokeDashoffset={offset}
+        strokeLinecap="round"
+        transform="rotate(-90 22 22)"
+      />
+      {/* D-Day 텍스트 */}
+      <text
+        x="50%"
+        y="50%"
+        textAnchor="middle"
+        font-family="KIMM_Bold"
+        fontStyle="normal"
+        dy=".3em"
+        fontSize="12px"
+        fontWeight="400"
+        fill="#F76241"
+      >
+        D-07
+      </text>
+    </svg>
+  );
+};
+
+export default CircularProgress;
--- src/components/ProjectManagement/Common/ProjectCard.js
+++ src/components/ProjectManagement/Common/ProjectCard.js
@@ -6,56 +6,7 @@ import avatar3 from "../../../assets/icons/avatar3.png";
 import avatar4 from "../../../assets/icons/avatar4.png";
 import { useNavigate } from "react-router-dom";
 import { IoCalendarOutline, IoTimeOutline } from "react-icons/io5";
-
-// 원형 프로그레스 바
-const CircularProgress = ({ percentage }) => {
-  const radius = 20;
-  const strokeWidth = 4;
-  const circumference = 2 * Math.PI * radius;
-  const offset = circumference - (percentage / 100) * circumference;
-
-  return (
-    <svg width="64" height="64" viewBox="0 0 44 44">
-      {/* 배경 원 */}
-      <circle
-        cx="22"
-        cy="22"
-        r={radius}
-        fill="none"
-        stroke="#eee"
-        strokeWidth={strokeWidth}
-      />
-      {/* 진행 바 */}
-      <circle
-        cx="22"
-        cy="22"
-        r={radius}
-        fill="none"
-        stroke="#F76241"
-        strokeWidth={strokeWidth}
-        strokeDasharray={circumference}
-        strokeDashoffset={offset}
-        strokeLinecap="round"
-        transform="rotate(-90 22 22)"
-      />
-      {/* D-Day 텍스트 */}
-      <text
-        x="50%"
-        y="50%"
-        textAnchor="middle"
-        font-family="KIMM_Bold"
-        fontStyle="normal"
-        dy=".3em"
-        fontSize="12px"
-        fontWeight="400"
-        fill="#F76241"
-      >
-        D-07
-      </text>
-    </svg>
-  );
-};
-
+import CircularProgress from "../../Common/CircularProgress";
 const ProjectCard = () => {
   const navigate = useNavigate();
 
