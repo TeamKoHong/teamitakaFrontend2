3512c28cba7e9d9130b5f987a3302f30695da183
_woo_s.j <sjwoo1999@korea.ac.kr>
2025-09-24 11:38:18 +0900

    fix(route): guard /main and /my with ProtectedRoute; ensure logout navigates to /login

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ import MainPage from './components/Home/MainPage';
 import { AuthProvider } from './contexts/AuthContext';
 import ToastHost from './components/Common/ToastHost';
 import AuthEventBridge from './components/Common/AuthEventBridge';
-import { PublicRoute } from './components/ProtectedRoute';
+import ProtectedRoute, { PublicRoute } from './components/ProtectedRoute';
 
 // 라우팅 상수 임포트
 import { 
@@ -232,9 +232,9 @@ const App = () => {
           <Route path={MAIN_ROUTES.LOGIN} element={<PublicRoute><LoginPage /></PublicRoute>} />
           <Route path={MAIN_ROUTES.REGISTER} element={<PublicRoute><RegisterPage /></PublicRoute>} />
 
-          {/* ===== 메인 페이지 (로그인 제한 없음) ===== */}
-          <Route path={MAIN_ROUTES.MAIN} element={<MainPage />} />
-          <Route path={MAIN_ROUTES.MY} element={<ProfilePage />} />
+          {/* ===== 메인/프로필 (인증 필요) ===== */}
+          <Route path={MAIN_ROUTES.MAIN} element={<ProtectedRoute><MainPage /></ProtectedRoute>} />
+          <Route path={MAIN_ROUTES.MY} element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
 
           {/* ===== 프로젝트 관리 라우트 (로그인 제한 없음) ===== */}
           <Route path={PROJECT_ROUTES.MANAGEMENT} element={<ProjectManagement />} />
